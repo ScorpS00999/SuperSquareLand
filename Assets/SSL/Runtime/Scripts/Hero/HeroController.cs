9f1ec03ab825b152bd39b493c27fe6353beed13a
Margot <margotdelage02@gmail.com>
2024-04-22 11:53:14 +0200

    Initial commit

---
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    [Header("Entity")]
+    [SerializeField] private HeroEntity _entity;
+
+    [Header("Debug")]
+    [SerializeField] private bool _guiDebug = false;
+
+    private void OnGUI()
+    {
+        if (!_guiDebug) return;
+
+        GUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label(gameObject.name);
+        GUILayout.EndVertical();
+    }
+
+    private float GetInputMoveX()
+    {
+        float inputMoveX = 0f;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q))
+        {
+            inputMoveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveX = 1f;
+        }
+
+        return inputMoveX;
+    }
+
+    private void Update()
+    {
+        _entity.SetMoveDirX(GetInputMoveX());
+    }
+}
