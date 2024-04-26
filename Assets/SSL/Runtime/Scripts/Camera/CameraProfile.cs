ff6bb91b30dc5d8821509bd86bce9c8610a57076
Margot <margotdelage02@gmail.com>
2024-04-26 12:32:39 +0200

    camera follow

--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -2,11 +2,27 @@
 
 public class CameraProfile : MonoBehaviour
 {
+    [Header("Type")]
+    [SerializeField] private CameraProfileType _profileType = CameraProfileType.Static;
+
+    [Header("Follow")]
+    [SerializeField] private Transform _targetToFollow = null;
+
+    public CameraProfileType ProfileType => _profileType;
+
+    public Transform TargetToFollow => _targetToFollow;
+
     private Camera _camera;
 
     public float CameraSize => _camera.orthographicSize;
 
     public Vector3 Position => _camera.transform.position;
+
+    public enum CameraProfileType
+    {
+        Static = 0,
+        FollowTarget
+    }
 
     private void Awake()
     {
